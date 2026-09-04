5d6d606143734a607c947a76ad0962ee4f84b87a
Vs-coder237 <kengne.bricejunior@ictuniversity.edu.cm>
2026-09-04 20:07:00 +0100

    connecting the project to prisma

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import app from "./src/app.js";
-const port = 3000
+import "dotenv/config"
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`the server is running on http://localhost:${port}`)
